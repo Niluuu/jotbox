27787e23c76c5230670ee71f8f3579ccf718930b
Niluuu <saidumarovanilufar@mail.ru>
2021-12-13 21:28:35 +0300

    feature(add reuseble function)

--- src/utils/editor/link.js
+++ src/utils/editor/link.js
@@ -1,12 +1,24 @@
-const Link = (props) => {
-    const {contentState, entityKey, children} = props
-    const {url} = contentState.getEntity(entityKey).getData();
-  
-    return ( 
-      <a href={url} className={styles.link} onClick={() => window.open(url)}>
-        {children}
-      </a>
-    );
-  };
+import styles from '../../modules/Editor/Editor.module.scss';
 
-export default Link
+
+export function findLinkEntities(contentBlock, callback, contentState) {
+  contentBlock.findEntityRanges((character) => {
+    const entityKey = character.getEntity();
+    return (
+      entityKey !== null &&
+      contentState.getEntity(entityKey).getType() === "LINK"
+    );
+  }, callback);
+}
+
+
+export const Link = (props) => {
+  const {contentState, entityKey, children} = props
+  const {url} = contentState.getEntity(entityKey).getData();
+
+  return ( 
+    <a href={url} className={styles.link} onClick={() => window.open(url)}>
+      {children}
+    </a>
+  );
+};

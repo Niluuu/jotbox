128fa499793c2493f3c30649d7ce406aec31ce87
Niluuu <saidumarovanilufar@mail.ru>
2021-12-15 20:39:45 +0300

    feature(rewrite js file)

---
+++ src/modules/Editor/MainEditor.tsx
@@ -0,0 +1,187 @@
+import { Component, useState, ReactElement, useRef } from 'react';
+import { EditorState, RichUtils, convertToRaw } from 'draft-js';
+import Editor from '@draft-js-plugins/editor';
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  BlockquoteButton,
+  CodeBlockButton,
+} from '@draft-js-plugins/buttons';
+import createHashtagPlugin from '@draft-js-plugins/hashtag';
+import createLinkPlugin from '@draft-js-plugins/anchor';
+import createInlineToolbarPlugin from '@draft-js-plugins/inline-toolbar';
+import jsonBeautify from 'json-beautify';
+import { linkifyPlugin } from '../../utils/editor/addLink';
+import { findLinkEntities, Link } from '../../utils/editor/link';
+import Modal from '../../component/modal/Modal';
+
+import styles from './Editor.module.scss';
+import '@draft-js-plugins/hashtag/lib/plugin.css';
+import 'draft-js/dist/Draft.css';
+import '@draft-js-plugins/inline-toolbar/lib/plugin.css';
+
+const linkPlugin = createLinkPlugin();
+const hashtagPlugin = createHashtagPlugin();
+const inlineToolbarPlugin = createInlineToolbarPlugin();
+
+const { InlineToolbar } = inlineToolbarPlugin;
+
+const customPlugin = {
+  decorators: [
+    {
+      strategy: findLinkEntities,
+      component: Link,
+    },
+  ],
+};
+
+const plugins = [linkifyPlugin, hashtagPlugin, customPlugin, inlineToolbarPlugin, linkPlugin];
+
+function MainEditor(props): ReactElement {
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+  const [urlValue, seturlValue] = useState('initialState');
+  const ref = useRef<Editor>(null);
+
+  const logState = () => {
+    const text = editorState.getCurrentContent().getBlocksAsArray();
+    const finalText = text.map((item) => item.getText());
+
+    console.log('finalTextt', finalText[0].slice(-2) === '[[');
+
+    if (finalText[0].slice(-2) === '[[' || finalText[0].slice(-2) === ']]') {
+      const start = finalText[0].indexOf('[[');
+      const end = finalText[0].indexOf(']]');
+    }
+  };
+
+  const onChange = (newEditorState) => {
+    setEditorState(newEditorState);
+    logState();
+  };
+
+  const onURLChange = (e) => seturlValue(e.target.value);
+
+  const promptForLink = (e) => {
+    e.preventDefault();
+
+    const selection = editorState.getSelection();
+
+    if (!selection.isCollapsed()) {
+      const contentState = editorState.getCurrentContent();
+      const startKey = editorState.getSelection().getStartKey();
+      const startOffset = editorState.getSelection().getStartOffset();
+      const blockWithLinkAtBeginning = contentState.getBlockForKey(startKey);
+      const linkKey = blockWithLinkAtBeginning.getEntityAt(startOffset);
+
+      let url = '';
+      if (linkKey) {
+        const linkInstance = contentState.getEntity(linkKey);
+        url = linkInstance.getData().url;
+      }
+
+      seturlValue(url);
+    }
+  };
+
+  const confirmLink = (e) => {
+    e.preventDefault();
+
+    const contentState = editorState.getCurrentContent();
+    const contentStateWithEntity = contentState.createEntity('LINK', 'MUTABLE', { url: urlValue });
+    const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+
+    // Apply entity
+    let nextEditorState = EditorState.set(editorState, { currentContent: contentStateWithEntity });
+
+    // Apply selection
+    nextEditorState = RichUtils.toggleLink(
+      nextEditorState,
+      nextEditorState.getSelection(),
+      entityKey,
+    );
+
+    setEditorState(nextEditorState)
+    seturlValue('')
+  };
+
+  const onLinkInputKeyDown = (e) => {
+    if (e.which === 13) {
+      confirmLink(e);
+    }
+  };
+
+  const removeLink = (e) => {
+    e.preventDefault();
+
+    const selection = editorState.getSelection();
+    if (!selection.isCollapsed()) setEditorState(RichUtils.toggleLink(editorState, selection, null))
+  };
+
+  const { linkMode, onLinkMode } = props;
+  const contentState = editorState.getCurrentContent();
+  const raw = convertToRaw(contentState);
+  const rawStr = jsonBeautify(raw, null, 2, 50);
+
+  return (
+    <div>
+      {/* <pre><code>{rawStr}</code></pre> */}
+      <div
+        className={styles.editor}
+        onClick={() => {
+          ref.current!.focus();
+        }}
+      >
+        <Editor
+          editorState={editorState}
+          onChange={onChange}
+          plugins={plugins}
+          ref={ref}
+        />
+        <InlineToolbar>
+          {(externalProps) => (
+            <>
+              <BoldButton {...externalProps} />
+              <ItalicButton {...externalProps} />
+              <UnderlineButton {...externalProps} />
+              <CodeButton {...externalProps} />
+              <UnorderedListButton {...externalProps} />
+              <OrderedListButton {...externalProps} />
+              <BlockquoteButton {...externalProps} />
+              <CodeBlockButton {...externalProps} />
+              <linkPlugin.LinkButton {...externalProps} />
+            </>
+          )}
+        </InlineToolbar>
+      </div>
+      <Modal isOpen={linkMode}>
+        <div className={styles.linkWrapper}>
+          <div className={styles.inputs}>
+            <input
+              onChange={onURLChange}
+              type="text"
+              value={urlValue}
+              onKeyDown={onLinkInputKeyDown}
+            />
+            <button type="button" onClick={onLinkMode} onMouseDown={confirmLink}>
+              Confirm
+            </button>
+          </div>
+          <div className={styles.buttons}>
+            <button type="button" onMouseDown={promptForLink}>
+              Add Link
+            </button>
+            <button type="button" onMouseDown={removeLink}>
+              Remove Link
+            </button>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+export default MainEditor;

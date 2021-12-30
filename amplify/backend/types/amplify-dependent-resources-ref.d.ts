aa56ca86e6e7d09e63d866dfa4f2fc8dde9a3d8b
TemurmalikReact <TemurmalikReact>
2021-12-30 16:40:10 +0500

    fixed error message

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+      "jotbox": {
+          "GraphQLAPIIdOutput": "string",
+          "GraphQLAPIEndpointOutput": "string"
+      }
+  },
+  "auth": {
+      "jotbox": {
+          "IdentityPoolId": "string",
+          "IdentityPoolName": "string",
+          "UserPoolId": "string",
+          "UserPoolArn": "string",
+          "UserPoolName": "string",
+          "AppClientIDWeb": "string",
+          "AppClientID": "string"
+      }
+  },
+  "storage": {
+      "s3jotboxstoraged17oh96r8b4gc1": {
+          "BucketName": "string",
+          "Region": "string"
+      }
+  }
+}

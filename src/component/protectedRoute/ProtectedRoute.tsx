086d6d564e5b6bc5adf328176d1fbc099cceac46
Niluuu <saidumarovanilufar@mail.ru>
2022-01-10 17:42:13 +0300

    bugfix(router )

--- src/component/protectedRoute/ProtectedRoute.tsx
+++ src/component/protectedRoute/ProtectedRoute.tsx
@@ -1,16 +1,13 @@
-import { Redirect, Route } from "react-router-dom";
+import { Redirect, Route } from 'react-router-dom';
+import SignInPage from '../../modules/SignInPage/SignInPage';
 
 function ProtectedRoute({ component: Component, ...restOfProps }) {
-  const isAuthenticated = localStorage.getItem("assessToken");
-/* eslint-disable react/jsx-props-no-spreading */
+  const isAuthenticated = localStorage.getItem('assessToken');
+  /* eslint-disable react/jsx-props-no-spreading */
   return (
     <Route
       {...restOfProps}
-      render={(props) =>
-        // isAuthenticated ? 
-        <Component {...props} /> 
-        // : <Redirect to="/signin" />
-      }
+      render={(props) => (isAuthenticated ? <Component {...props} /> : <SignInPage />)}
     />
   );
 }

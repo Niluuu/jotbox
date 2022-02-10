5e55144b4254c1125ffc5394ea1937ba06f6953f
Niluuu <saidumarovanilufar@mail.ru>
2022-02-10 15:50:59 +0300

    bug(editor): mention fix

--- src/modules/Editor/MainEditor.tsx
+++ src/modules/Editor/MainEditor.tsx
@@ -68,7 +68,7 @@ const MainEditor: FC<MainEditorProps> = ({
 
     listNodes.filter(
       // eslint-disable-next-line no-underscore-dangle
-      (node) => node._deleted !== null && mention.push({ name: node.title, link: node.id }),
+      (node) => node._deleted !== true && mention.push({ name: node.title, link: node.id }),
     );
 
     return mention;

94a72a11a93ee83255bdf4a3eec43bef5ea8c015
Niluuu <saidumarovanilufar@mail.ru>
2022-05-23 10:22:54 +0300

    feat: backend storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -17,5 +17,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "storage": {
+        "s3jotboxstorage27261fdb": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }

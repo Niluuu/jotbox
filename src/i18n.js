69b7340fbe97c3d5c04a5eda5ba621bb54f0a38e
TemurmalikReact <TemurmalikReact>
2022-05-23 16:16:03 +0500

    fix: created translation.json files for useTranslation

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,20 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { resources } from './utils/translations';
+import en from '../public/locales/en/translation.json';
+import ru from '../public/locales/ru/translation.json';
 
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+
+const resources = {
+  en: {
+    translation: en,
+  },
+  ru: {
+    translation: ru,
+  },
+};
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
